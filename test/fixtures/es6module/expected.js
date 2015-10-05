91d347debadf803d9b8ca54b19db727c0a739ca2
Takuto Wada <takuto.wada@gmail.com>
2015-10-05 22:32:52 +0900

    test(unassert): add failing test for ES6 import removal feature

---
+++ test/fixtures/es6module/expected.js
@@ -0,0 +1,5 @@
+'use strict';
+
+function add(a, b) {
+    return a + b;
+}
---
+++ test/fixtures/es6module/fixture.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import assert from 'assert';
+
+function add (a, b) {
+    assert(!isNaN(a));
+    assert.equal(typeof b, 'number');
+    assert.ok(!isNaN(b));
+    return a + b;
+}
