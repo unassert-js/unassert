8deeae3c31be13b0a17a83997150a102a030f78b
Takuto Wada <takuto.wada@gmail.com>
2022-07-18 16:50:11 +0900

    feat: replace AST matcher with simpler and robust logic
    since AST deep-equality introduces extra dependencies
    
    BREAKING CHANGE:
    
    configuration options are simplified.
    patterns are decomposed into `variables` and `modules`.
    
    + {
    +   variables: [
    +     'assert'
    +   ],
    +   modules: [
    +     'assert',
    +     'power-assert',
    +     'node:assert'
    +   ]
    + }
    
    - {
    -     assertionPatterns: [
    -         'assert(value, [message])',
    -         'assert.ok(value, [message])',
    -         'assert.equal(actual, expected, [message])',
    -         'assert.notEqual(actual, expected, [message])',
    -         'assert.strictEqual(actual, expected, [message])',
    -         'assert.notStrictEqual(actual, expected, [message])',
    -         'assert.deepEqual(actual, expected, [message])',
    -         'assert.notDeepEqual(actual, expected, [message])',
    -         'assert.deepStrictEqual(actual, expected, [message])',
    -         'assert.notDeepStrictEqual(actual, expected, [message])',
    -         'assert.fail(actual, expected, message, operator)',
    -         'assert.throws(block, [error], [message])',
    -         'assert.doesNotThrow(block, [message])',
    -         'assert.ifError(value)',
    -         'console.assert(value, [message])'
    -     ],
    -     requirePatterns: [
    -         'assert = require("assert")',
    -         'assert = require("power-assert")'
    -     ],
    -     importPatterns: [
    -         'import assert from "assert"',
    -         'import * as assert from "assert"',
    -         'import assert from "power-assert"',
    -         'import * as assert from "power-assert"'
    -     ]
    - }

--- index.js
+++ index.js
@@ -13,17 +13,6 @@
 var estraverse = require('estraverse');
 var syntax = estraverse.Syntax;
 var esutils = require('esutils');
-var objectAssign = require('object-assign');
-var defaultOptions = require('./lib/default-options');
-var createCallMatcher = require('./lib/create-call-matcher');
-var createRequireMatcher = require('./lib/create-require-matcher');
-var createImportMatcher = require('./lib/create-import-matcher');
-
-function matches (node) {
-    return function (matcher) {
-        return matcher.test(node);
-    };
-}
 
 function isBodyOfIfStatement (parentNode, key) {
     return parentNode.type === syntax.IfStatement && (key === 'consequent' || key === 'alternate');
@@ -39,30 +28,139 @@ function isNonBlockChildOfIfStatementOrLoop (currentNode, parentNode, key) {
         (isBodyOfIfStatement(parentNode, key) || isBodyOfIterationStatement(parentNode, key));
 }
 
-function compileMatchers (options) {
-    var config = objectAssign(defaultOptions(), options);
-    return {
-        imports: config.importPatterns.map(createImportMatcher),
-        requires: config.requirePatterns.map(createRequireMatcher),
-        assertions: config.assertionPatterns.map(createCallMatcher)
-    };
+function isLiteral(node) {
+  return node && node.type == syntax.Literal;
+}
+function isIdentifier(node) {
+  return node && node.type == syntax.Identifier;
+}
+function isMemberExpression(node) {
+  return node && node.type == syntax.MemberExpression;
+}
+function isCallExpression(node) {
+  return node && node.type == syntax.CallExpression;
+}
+function isExpressionStatement(node) {
+  return node && node.type == syntax.ExpressionStatement;
+}
+function isImportDefaultSpecifier(node) {
+  return node && node.type == syntax.ImportDefaultSpecifier;
+}
+function isImportNamespaceSpecifier(node) {
+  return node && node.type == syntax.ImportNamespaceSpecifier;
+}
+function isImportSpecifier(node) {
+  return node && node.type == syntax.ImportSpecifier;
 }
 
-function createVisitorByMatchers (matchers) {
+
+function createVisitor (options) {
+
+const config = Object.assign({
+  variables: [
+    'assert'
+  ],
+  modules: [
+    'assert',
+    'power-assert',
+    'node:assert'
+  ]
+}, options);
+
+function isAssertionModuleName (lit) {
+  return config.modules.some((name) => lit.value === name);
+}
+
+function isAssertionVariableName (id) {
+  if (!isIdentifier(id)) {
+    return false;
+  }
+  return config.variables.some((name) => id.name === name);
+}
+
+function isAssertionMethod (callee) {
+  if (!isMemberExpression(callee)) {
+    return false;
+  }
+  const obj = callee.object;
+  if (isMemberExpression(obj)) {
+    return isAssertionMethod(obj);
+  } else {
+    return isAssertionVariableName(obj);
+  }
+}
+
+function isAssertionFunction (callee) {
+  return isAssertionVariableName(callee);
+}
+
+function isConsoleAssert(callee) {
+  if (!isMemberExpression(callee)) {
+    return false;
+  }
+  const obj = callee.object;
+  const prop = callee.property;
+  return isIdentifier(obj) && obj.name === 'console'
+      && isIdentifier(prop) && prop.name === 'assert';
+}
+
+const isRequireAssert = (id, init) => {
+  if (!isIdentifier(id)) {
+    return false;
+  }
+  if (!isAssertionVariableName(id)) {
+    return false;
+  }
+  if (!isCallExpression(init)) {
+    return false;
+  }
+  const callee = init.callee;
+  if (!isIdentifier(callee) || callee.name !== 'require') {
+    return false;
+  }
+  const arg = init.arguments[0];
+  return (isLiteral(arg) && isAssertionModuleName(arg));
+};
+
+const isRequireAssertStrict = (id, init) => {
+  if (!isMemberExpression(init)) {
+    return false;
+  }
+  if (!isRequireAssert(id, init.object)) {
+    return false;
+  }
+  const prop = init.property;
+  if (!isIdentifier(prop)) {
+    return false;
+  }
+  return prop.name === 'strict';
+};
+
+const isRemovalTarget = (id, init) => isRequireAssert(id, init) || isRequireAssertStrict(id, init);
+
     var pathToRemove = {};
     return {
         enter: function (currentNode, parentNode) {
             var espathToRemove;
             switch (currentNode.type) {
             case syntax.ImportDeclaration:
-                if (matchers.imports.some(matches(currentNode))) {
+                const source = currentNode.source;
+                if (!(isAssertionModuleName(source))) {
+                    return;
+                }
+                const firstSpecifier = currentNode.specifiers[0];
+                if (!(isImportDefaultSpecifier(firstSpecifier) || isImportNamespaceSpecifier(firstSpecifier) || isImportSpecifier(firstSpecifier))) {
+                    return;
+                }
+                const local = firstSpecifier.local;
+                if (isAssertionVariableName(local)) {
                     espathToRemove = this.path().join('/');
                     pathToRemove[espathToRemove] = true;
                     this.skip();
                 }
                 break;
             case syntax.VariableDeclarator:
-                if (matchers.requires.some(matches(currentNode))) {
+                if (isRemovalTarget(currentNode.id, currentNode.init)) {
                     if (parentNode.declarations.length === 1) {
                         // remove parent VariableDeclaration
                         // body/1/declarations/0 -> body/1
@@ -76,8 +174,13 @@ function createVisitorByMatchers (matchers) {
                 }
                 break;
             case syntax.AssignmentExpression:
-                if (parentNode.type === syntax.ExpressionStatement &&
-                    matchers.requires.some(matches(currentNode))) {
+                if (currentNode.operator !== '=') {
+                    return;
+                }
+                if (!isExpressionStatement(parentNode)) {
+                    return;
+                }
+                if (isRemovalTarget(currentNode.left, currentNode.right)) {
                     // remove parent ExpressionStatement
                     espathToRemove = this.path().slice(0, -1).join('/');
                     pathToRemove[espathToRemove] = true;
@@ -85,8 +188,11 @@ function createVisitorByMatchers (matchers) {
                 }
                 break;
             case syntax.CallExpression:
-                if (parentNode.type === syntax.ExpressionStatement &&
-                    matchers.assertions.some(matches(currentNode))) {
+                if (!isExpressionStatement(parentNode)) {
+                    return;
+                }
+                const callee = currentNode.callee;
+                if (isAssertionFunction(callee) || isAssertionMethod(callee) || isConsoleAssert(callee)) {
                     // remove parent ExpressionStatement
                     // body/1/body/body/0/expression -> body/1/body/body/0
                     espathToRemove = this.path().slice(0, -1).join('/');
@@ -114,16 +220,9 @@ function createVisitorByMatchers (matchers) {
     };
 }
 
-var defaultMatchers = compileMatchers();
-
 function unassert (ast) {
-    return estraverse.replace(ast, createVisitorByMatchers(defaultMatchers));
+    return estraverse.replace(ast, createVisitor());
 }
 
-function createVisitor (options) {
-    return createVisitorByMatchers(compileMatchers(options));
-}
-
-unassert.defaultOptions = defaultOptions;
 unassert.createVisitor = createVisitor;
 module.exports = unassert;
