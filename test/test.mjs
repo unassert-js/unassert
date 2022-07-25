4e27031e3ceeaa99c461f74b3bb5721a439cb404
Takuto Wada <takuto.wada@gmail.com>
2022-07-25 16:06:25 +0900

    docs(README): add ESM code example

--- test/test.mjs
+++ test/test.mjs
@@ -6,8 +6,7 @@ import { parse } from 'acorn';
 import { generate } from 'escodegen';
 import { replace } from 'estraverse';
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 function parseFixture (filepath) {
   return parse(readFileSync(filepath), { sourceType: 'module', ecmaVersion: '2022' });
